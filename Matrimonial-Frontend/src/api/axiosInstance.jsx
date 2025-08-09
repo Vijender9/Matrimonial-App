c14eeb238a70eabf7e1ad512f507e092c6bfbe3d
Vijender9 <viju0463@gmail.com>
2025-08-09 15:06:57 +0530

    adding render url in frontend

--- Matrimonial-Frontend/src/api/axiosInstance.jsx
+++ Matrimonial-Frontend/src/api/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API=axios.create({
-    baseURL:"http://localhost:1000/api"
+    baseURL:"https://matrimonial-app-m0oi.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
