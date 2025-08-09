9e5dbe9c9e5ff576fbaae2c921461e4c62cbdc73
Vijender9 <viju0463@gmail.com>
2025-08-09 15:12:54 +0530

    updated vercel url in backend

--- Matrimonial-Backend/src/index.js
+++ Matrimonial-Backend/src/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app=express();
 app.use(cors({
 
-    origin:"https://matrimonial-g2v1et5o8-vijenders-projects-e68981af.vercel.app/",
+    origin:"https://matrimonial-g2v1et5o8-vijenders-projects-e68981af.vercel.app",
     credentials:true
 }))
 app.use(express.json());
